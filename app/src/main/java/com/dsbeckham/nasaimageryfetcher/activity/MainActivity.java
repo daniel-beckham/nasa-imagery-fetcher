7317282f38113c59b16afe83f1910aeab5fc6caa
dsbeckham <beckhamdaniels@gmail.com>
2016-04-26 01:02:16 -0400

    Make ToolBar and status bar transparent in ViewPager

--- app/src/main/java/com/dsbeckham/nasaimageryfetcher/activity/MainActivity.java
+++ app/src/main/java/com/dsbeckham/nasaimageryfetcher/activity/MainActivity.java
@@ -1,9 +1,6 @@
 package com.dsbeckham.nasaimageryfetcher.activity;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
@@ -13,8 +10,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
 
 import com.dsbeckham.nasaimageryfetcher.R;
 import com.dsbeckham.nasaimageryfetcher.fragment.ApodFragment;
@@ -30,30 +25,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     DrawerLayout drawerLayout;
     @Bind(R.id.nav_view)
     NavigationView navigationView;
-    @Bind(R.id.toolbar)
+    @Bind(R.id.main_toolbar)
     Toolbar toolbar;
 
-    @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        navigationView.setNavigationItemSelectedListener(this);
         setSupportActionBar(toolbar);
 
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
         actionBarDrawerToggle.syncState();
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-                getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            } else {
-                getWindow().setStatusBarColor(Color.TRANSPARENT);
-            }
-        }
+        navigationView.setNavigationItemSelectedListener(this);
 
         PreferenceUtils.setDefaultValuesForPreferences(this);
         QueryUtils.setUpIoServices();
--- app/src/main/java/com/dsbeckham/nasaimageryfetcher/activity/ViewPagerActivity.java
+++ app/src/main/java/com/dsbeckham/nasaimageryfetcher/activity/ViewPagerActivity.java
@@ -1,11 +1,17 @@
 package com.dsbeckham.nasaimageryfetcher.activity;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.dsbeckham.nasaimageryfetcher.R;
 import com.dsbeckham.nasaimageryfetcher.adapter.ImageFragmentStatePagerAdapter;
@@ -19,6 +25,8 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ViewPagerActivity extends AppCompatActivity {
+    @Bind(R.id.viewpager_toolbar)
+    Toolbar toolbar;
     @Bind(R.id.viewpager)
     public ViewPager viewPager;
 
@@ -28,11 +36,28 @@ public class ViewPagerActivity extends AppCompatActivity {
 
     static final String VIEW_PAGER_CURRENT_ITEM = "viewPagerCurrentItem";
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewpager);
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            } else {
+                getWindow().setStatusBarColor(Color.TRANSPARENT);
+            }
+        }
 
         currentFragment = PreferenceManager.getDefaultSharedPreferences(this).getString(PreferenceUtils.PREF_CURRENT_FRAGMENT, "iotd");
 
